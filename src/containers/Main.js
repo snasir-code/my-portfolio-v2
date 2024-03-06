b30181d2f0604826c68d707664ea66f9d258c8f6
Shahzaib Nasir <shah.nasir@outlook.com>
2024-03-06 16:32:13 +0000

    changed html index file

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,7 +3,6 @@ import Header from "../components/header/Header";
 import Greeting from "./greeting/Greeting";
 import Skills from "./skills/Skills";
 import WorkExperience from "./workExperience/WorkExperience";
-import Projects from "./projects/Projects";
 import Footer from "../components/footer/Footer";
 import Top from "./topbutton/Top";
 import Profile from "./profile/Profile";
